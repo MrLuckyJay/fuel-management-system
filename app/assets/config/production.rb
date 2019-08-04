c3bbc610274094b4fd38e65b5d7226571dafaf11
MrLuckyJay <nabieutitusjusu@gmail.com>
2019-08-04 02:32:02 +0000

    added a p.rb file

---
+++ app/assets/config/production.rb
@@ -0,0 +1,7 @@
+APPLICATIONNAME::Application.configure do                        
+    config.cache_classes = true
+    config.serve_static_assets = true
+    config.assets.compile = true
+    config.assets.digest = true
+    
+end
