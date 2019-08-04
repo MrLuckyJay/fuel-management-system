28822877147c26e1e4c658f9a75a47d55ee12254
MrLuckyJay <nabieutitusjusu@gmail.com>
2019-08-04 02:41:10 +0000

    Fix

--- app/assets/config/production.rb
+++ app/assets/config/production.rb
@@ -1,6 +1,4 @@
 APPLICATIONNAME::Application.configure do                        
-    config.serve_static_assets = true
     config.assets.compile = true
-    config.assets.digest = true
     
 end
