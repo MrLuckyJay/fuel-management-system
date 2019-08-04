0352c51408222e5c71364c9f8dce28e5b0c87d78
MrLuckyJay <nabieutitusjusu@gmail.com>
2019-08-04 01:42:51 +0000

    Configured the signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user, except: [ :login,:new]
+  before_action :require_user, except: [ :login,:new,:create]
 
 
   # GET /users
